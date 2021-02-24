05d18f5de44d2a0a60a9c250c3b0a6163c7f1ff1
supakitwongkaew <supakit.wo.61@ubu.ac.th>
2021-02-24 19:21:15 +0700

    secon

---
+++ src/components/SidebarData.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as GiWhiteIcons from 'react-icons/gi';
+export const SidebarData = [
+
+  {
+    title: 'บทที่ 1',
+    path: '/chapter1',
+    icon: <GiWhiteIcons.GiWhiteBook />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'บทที่ 2',
+    path: '/#',
+    icon: <GiWhiteIcons.GiWhiteBook />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'บทที่ 3',
+    path: '/#',
+    icon: <GiWhiteIcons.GiWhiteBook />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'บทที่ 4',
+    path: '/#',
+    icon: <GiWhiteIcons.GiWhiteBook />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'บทที่ 5',
+    path: '/#',
+    icon: <GiWhiteIcons.GiWhiteBook />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'บทที่ 6',
+    path: '/#',
+    icon: <GiWhiteIcons.GiWhiteBook />,
+    cName: 'nav-text'
+  }
+];
